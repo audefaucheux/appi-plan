37ec193935f070c25f4876681f6448be05bb722f
Aude Faucheux <aude53@gmail.com>
2021-02-23 22:23:08 +0000

    Revert "style RecipeCard"
    This reverts commit a1aed059d3f9f788f0c8af0c1433b26a90dd249b.

--- src/Components/Recipes/RecipeCard.tsx
+++ src/Components/Recipes/RecipeCard.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Recipe from "../../Types/Recipe";
 import RecipePreview from "./RecipePreview";
 import placeholderImage from "../../images/placeholder-image.png";
-import { Box, Image } from "@chakra-ui/react";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -12,30 +11,11 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
   const [openPreview, setOpenPreview] = useState<boolean>(false);
 
   return (
-    <Box
-      maxW="sm"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      _hover={{
-        background: "#E8E8E8",
-      }}
-      onClick={() => setOpenPreview(true)}
-    >
-      <Image p={[2]} w="100%" src={placeholderImage} alt="placeholderImage" />
-      <Box p={[2]}>
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          <div>{recipe.title}</div>
-          {openPreview && <RecipePreview {...{ recipe, setOpenPreview }} />}
-        </Box>
-      </Box>
-    </Box>
+    <div className="recipe-card" onClick={() => setOpenPreview(true)}>
+      <img src={placeholderImage} alt="placeholderImage" />
+      <div>{recipe.title}</div>
+      {openPreview && <RecipePreview {...{ recipe, setOpenPreview }} />}
+    </div>
   );
 };
 
--- src/Components/Recipes/RecipeList.tsx
+++ src/Components/Recipes/RecipeList.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Recipe from "../../Types/Recipe";
 import RecipeCard from "./RecipeCard";
-import { Heading, Input, Box, Flex } from "@chakra-ui/react";
+import { Heading, Input, Box } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { getRecipes } from "../../Services/AppiPlanBackend/Recipes";
 import { getIngredients } from "../../Services/AppiPlanBackend/Ingredients";
@@ -36,11 +36,11 @@ const RecipeList: React.FC = () => {
           icon={<SearchIcon />}
         />
       </Box>
-      <Flex wrap="wrap">
+      <div className="recipe-list">
         {filterRecipes(recipes).map((recipe, index) => (
           <RecipeCard key={index} recipe={recipe} />
         ))}
-      </Flex>
+      </div>
     </Box>
   );
 };
