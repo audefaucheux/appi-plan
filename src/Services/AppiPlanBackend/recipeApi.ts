027704823afd8bc77d7d2f5455b9fa6235d47dd7
Aude Faucheux <aude53@gmail.com>
2021-05-03 15:24:46 +0100

    add create recipe endpoint

--- src/Services/AppiPlanBackend/recipeApi.ts
+++ src/Services/AppiPlanBackend/recipeApi.ts
@@ -1,21 +1,42 @@
 import axios from "axios";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import Recipe from "../../Types/Recipe";
 
+const server = "http://localhost:3004";
+
 export const getRecipes = async (
-  setter: React.Dispatch<React.SetStateAction<Recipe[]>>
+  setter: Dispatch<SetStateAction<Recipe[]>>
 ): Promise<void> => {
   try {
-    const recipes = await axios.get("http://localhost:3004/recipes");
+    const recipes = await axios.get(`${server}/recipes`);
     setter(recipes.data);
   } catch (error) {
     console.log(error);
   }
 };
 
-export const updateRecipe = async (recipe: Recipe): Promise<void> => {
+export const createRecipe = async (
+  recipe: Partial<Recipe>,
+  setter: Dispatch<SetStateAction<Recipe | undefined>>
+): Promise<void> => {
   try {
-    await axios.patch(`http://localhost:3004/recipes/${recipe.id}`, recipe);
+    const newRecipe = await axios.post(`${server}/recipes`, recipe);
+    setter(newRecipe.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateRecipe = async (
+  recipe: Recipe,
+  setter: Dispatch<SetStateAction<Recipe | undefined>>
+): Promise<void> => {
+  try {
+    const updatedRecipe = await axios.patch(
+      `${server}/recipes/${recipe.id}`,
+      recipe
+    );
+    setter(updatedRecipe.data);
   } catch (error) {
     console.log(error);
   }
